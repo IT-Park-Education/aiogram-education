72b063d000f837cda4f92adeef21f9ebeec36957
Ergashev Baxtiyor <86454228+Baxtiyor98@users.noreply.github.com>
2023-01-28 21:57:41 +0500

    first

---
+++ utils/set_bot_commands.py
@@ -0,0 +1,11 @@
+from aiogram import types
+
+
+async def set_default_commands(dp):
+    await dp.bot.set_my_commands(
+        [
+            types.BotCommand("start", "Botni ishga tushurish"),
+            types.BotCommand("myid", "Mening telegram ID"),
+            types.BotCommand("help", "Yordam"),
+        ]
+    )
